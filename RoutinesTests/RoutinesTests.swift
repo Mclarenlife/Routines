93a5e1694ceb7ec31e1cc62106fc479f354e809c
Mclarenlife <mclarenlife@qq.com>
2025-07-19 13:52:02 +0800

    Initial Commit

---
+++ RoutinesTests/RoutinesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoutinesTests.swift
+//  RoutinesTests
+//
+//  Created by Mclarenlife on 2025/7/19.
+//
+
+import Testing
+@testable import Routines
+
+struct RoutinesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
